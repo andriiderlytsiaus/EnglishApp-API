5786707461ee54153cc444657d205bfc993a776f
Andrii Derlytsia <andriiderlytsia@gmail.com>
2024-01-08 15:55:24 -0800

    aws

--- BLL/obj/Debug/net6.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net6.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b224cccb3f7a1e6e73977bd14073a0106589fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
