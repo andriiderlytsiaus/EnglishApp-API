4b74d9f379013c3cb5a2cbfe88475c69ed6f9810
Andrii <andriiderlytsia@gmail.com>
2024-05-04 21:55:30 -0700

    change connection string

--- BLL/obj/Debug/net6.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net6.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f247843026fe93cf99ecf6c9e93136500200f002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd356a3d91661610d7181966168443b23b6a0640")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
