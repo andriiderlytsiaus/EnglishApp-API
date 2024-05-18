83a180487f22bad0565e72396e839293ab63c47a
Andrii <andriiderlytsia@gmail.com>
2024-05-18 11:25:40 -0700

    New Bb connection

--- EnglishApp/obj/Debug/net6.0/EnglishApp.AssemblyInfo.cs
+++ EnglishApp/obj/Debug/net6.0/EnglishApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b74d9f379013c3cb5a2cbfe88475c69ed6f9810")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b722402ae9a14c81f4ccf67fe110315ae0b6b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
